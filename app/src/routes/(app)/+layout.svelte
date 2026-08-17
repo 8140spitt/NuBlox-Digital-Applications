b0b4b22bf2fe831616c3d7bbdb6663ee9249578a
8140spitt <56647740+8140spitt@users.noreply.github.com>
2026-08-17 13:59:27 +0100

    Add collections navigation

--- app/src/routes/(app)/+layout.svelte
+++ app/src/routes/(app)/+layout.svelte
@@ -39,6 +39,7 @@
 			<a class="subnav" href="/finance/credit-notes">Credit notes</a>
 			<a class="subnav" href="/finance/payments">Payments</a>
 			<a class="subnav" href="/finance/receivables">Receivables</a>
+			<a class="subnav" href="/finance/collections">Collections</a>
 			<a class="subnav" href="/finance/billing">Billing settings</a>
 			<a href="/organisation">Organisation</a>
 		</nav>
