2e8fbd7642dcb164d736775a6b843fb4bb5e965c
8140spitt <56647740+8140spitt@users.noreply.github.com>
2026-08-18 13:44:04 +0100

    Expose accounting periods in finance navigation

--- app/src/routes/(app)/+layout.svelte
+++ app/src/routes/(app)/+layout.svelte
@@ -43,6 +43,7 @@
 			<a class="subnav" href="/finance/bad-debt">Bad debt</a>
 			<a class="subnav" href="/finance/tax-relief">VAT bad-debt relief</a>
 			<a class="subnav" href="/finance/accounting">Accounting</a>
+			<a class="subnav" href="/finance/accounting/periods">Accounting periods</a>
 			<a class="subnav" href="/finance/billing">Billing settings</a>
 			<a class="subnav" href="/finance/tax">Tax settings</a>
 			<a href="/organisation">Organisation</a>
