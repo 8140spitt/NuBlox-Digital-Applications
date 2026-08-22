46778ea2ffeefababc17975ce6231b4b92823d49
8140spitt <56647740+8140spitt@users.noreply.github.com>
2026-08-22 17:26:59 +0100

    Add teams organisation navigation

--- app/src/routes/(app)/organisation/+layout.svelte
+++ app/src/routes/(app)/organisation/+layout.svelte
@@ -13,6 +13,7 @@
 	<a href="/organisation/identity" class:active={pathname === '/organisation/identity'}>
 		Legal identity
 	</a>
+	<a href="/organisation/teams" class:active={pathname === '/organisation/teams'}>Teams</a>
 	<a href="/organisation/permissions" class:active={pathname === '/organisation/permissions'}>
 		Permission exceptions
 	</a>
