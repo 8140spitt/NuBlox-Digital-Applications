b4bcac084209a855497cd3a349d13db1a9139486
8140spitt <56647740+8140spitt@users.noreply.github.com>
2026-08-25 21:59:23 +0100

    feat(brand): export NuBlox brand components

---
+++ app/src/lib/components/brand/index.ts
@@ -0,0 +1,4 @@
+export { default as MarketingHeader } from './MarketingHeader.svelte';
+export { default as MarketingHero } from './MarketingHero.svelte';
+export { default as NuBloxLockup } from './NuBloxLockup.svelte';
+export { default as NuBloxMark } from './NuBloxMark.svelte';
